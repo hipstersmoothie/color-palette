aa66f79954182b916b0d0174c6a3ddd50530a62f
Andrew Lisowski <andrew_lisowski@intuit.com>
2018-11-12 19:15:43 -0800

    fix mobile version

--- pages/_document.js
+++ pages/_document.js
@@ -15,6 +15,7 @@ export default class MyDocument extends Document {
     return (
       <html>
         <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
           <style>{`body { margin: 0 } /* custom! */`}</style>
           <script
             defer
